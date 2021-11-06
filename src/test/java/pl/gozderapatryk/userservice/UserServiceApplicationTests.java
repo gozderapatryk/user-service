30a22f7867f7a6f12841848fef69b1acf8d30465
Patryk Gozdera <gozderapatryk@gmail.com>
2021-11-06 01:29:59 +0100

    Init project.

---
+++ src/test/java/pl/gozderapatryk/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gozderapatryk.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
